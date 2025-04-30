f6e25c02cb904433fee5aa52a3237744278b031c
lachieuk <mr.lachlan.fuller@gmail.com>
2025-04-30 16:01:07 +0100

    Update lachie_insurance_testing_app.py

--- lachie_insurance_testing_app.py
+++ lachie_insurance_testing_app.py
@@ -93,7 +93,7 @@ st.title("Lachie Insurance Testing App")
 st.header("Select Insurer")
 insurer_options = {
     "Aviva": 1000,
-    "Admiral": 2500,
+    "Admiral": 1220,
     "Wakam": 2000
 }
 insurer = st.radio("Insurer", list(insurer_options.keys()), index=0)
